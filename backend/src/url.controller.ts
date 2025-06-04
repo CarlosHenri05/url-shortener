c988308a8aa14e6358d3f06be41ef59962c618b4
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-06-04 20:31:56 -0300

    Adding controllers with post and get endpoints

--- backend/src/url.controller.ts
+++ backend/src/url.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Get } from '@nestjs/common';
+import { RequestUrlDto, ResponseUrlDto } from './dto/url.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { UrlService } from './url.service';
 
-@Controller('url')
-export class UrlController {}
+@Controller('api')
+export class UrlController {
+  constructor(private readonly urlService: UrlService) {}
+
+  @Post('/shorten')
+  createShortUrl(body: RequestUrlDto): Promise<void> {
+    return this.urlService.createShortUrl(body.getOriginalUrl());
+  }
+
+  @Get('/shorten')
+  getShortUrl(body: RequestUrlDto): Promise<ResponseUrlDto> {
+    return this.urlService.getShortUrl(body.getOriginalUrl());
+  }
+}
