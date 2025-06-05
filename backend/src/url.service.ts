d4515c96d66380a2e0a54928f603728f443c9e0a
CarlosHenri05 <carloshenrique20051bb@gmail.com>
2025-06-05 00:32:29 -0300

    Fixing getShortUrl method logic

--- backend/src/url.service.ts
+++ backend/src/url.service.ts
@@ -3,40 +3,47 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Url } from './models/url.models';
 import { Model } from 'mongoose';
 import { nanoid } from 'nanoid';
+import { UrlNotFound } from './utils/generic-errors';
 
 @Injectable()
 export class UrlService {
   constructor(@InjectModel(Url.name) private readonly urlModel: Model<Url>) {}
 
-  async createShortUrl(originalUrl: string): Promise<void> {
-    let findingUrl = await this.urlModel.findOne({ originalUrl: originalUrl });
+  async createShortUrl(originalUrl: string): Promise<Url> {
+    let findingUrl = await this.urlModel.findOne({ originalUrl });
 
     if (findingUrl) {
-      await this.getShortUrl(originalUrl);
+      return findingUrl;
     }
 
     try {
       const shortUrl = nanoid(10);
 
-      let newUrl = new this.urlModel({
+      const newUrl = new this.urlModel({
         originalUrl: originalUrl,
         shortUrl: shortUrl,
       }).save();
+
+      return newUrl;
     } catch (error) {
       console.error('Error :' + error);
       throw error;
     }
   }
 
-  async getShortUrl(originalUrl: string): Promise<Url> {
+  async getShortUrl(shortUrl: string): Promise<Url> {
     try {
-      let getUrl = await this.urlModel.findOne({
-        where: { originalUrl: originalUrl },
-      });
+      const model = await this.urlModel.findOne({ shortUrl: shortUrl });
 
-      return <Url>getUrl;
+      if (model) {
+        return model;
+      }
+
+      throw new UrlNotFound(
+        'Error url not found',
+        'Url was not found in the database. Try again or use the post method first.',
+      );
     } catch (error) {
-      console.error('Error: ' + error);
       throw error;
     }
   }
